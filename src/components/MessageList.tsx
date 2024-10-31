c55cf7966320f67cb38504797844f3157ebb7d5c
Arogya Bichpuria <arogyabichpuria16@gmail.com>
2024-10-31 21:00:17 +0530

    added MessageList comp

---
+++ src/components/MessageList.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, View} from 'react-native';
+import {Colors} from '../colors';
+
+type MessageListProps = {
+  messages: Message[];
+};
+export const MessageList = ({messages}: MessageListProps) => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: Colors.black},
+});
